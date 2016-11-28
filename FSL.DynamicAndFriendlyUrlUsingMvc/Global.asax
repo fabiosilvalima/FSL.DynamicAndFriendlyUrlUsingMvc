3399dbd22c019ec473f03edab6428e828afb2704
Fabio Silva Lima <fabiosilvalima@terra.com.br>
2016-11-28 11:08:58 -0200

    primeiro commit

---
+++ FSL.DynamicAndFriendlyUrlUsingMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FSL.DynamicAndFriendlyUrlUsingMvc.MvcApplication" Language="C#" %>
---
+++ FSL.DynamicAndFriendlyUrlUsingMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FSL.DynamicAndFriendlyUrlUsingMvc.Startup))]
+namespace FSL.DynamicAndFriendlyUrlUsingMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
